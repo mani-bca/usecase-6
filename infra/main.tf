0cb4d545fa274d446f3053212eed0562b0b3d88a
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-10 14:06:24 +0000

    fedbghn

--- infra/main.tf
+++ infra/main.tf
@@ -41,7 +41,7 @@ module "cloudwatch" {
 
 # Create Lambda functions with CloudWatch Event Rule ARNs
 module "lambda_start" {
-  source = "./modules/lambda"
+  source = "git::https://github.com/mani-bca/set-aws-infra.git//modules/schedule2/lambda?ref=main"
 
   project_name          = var.project_name
   environment           = var.environment
@@ -61,7 +61,7 @@ module "lambda_start" {
 }
 
 module "lambda_stop" {
-  source = "./modules/lambda"
+  source = "git::https://github.com/mani-bca/set-aws-infra.git//modules/schedule2/lambda?ref=main"
 
   project_name          = var.project_name
   environment           = var.environment
